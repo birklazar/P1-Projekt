9512cae4bb188f4aa7dc05efa96835bf4895f5f6
ZnakeDenmark <victor.dyhrman@gmail.com>
2025-10-28 11:57:29 +0100

    Opdateret main.c

--- Main.c
+++ Main.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
-FILE *Ingredients;
+
 int main() {
+    FILE *Ingredients;
     char input[1000];
     char ingredients[1000];
     double maengde=0;
+
+
     printf("Write an ingredient and an amount in grams: \n");
     scanf("%s%lf",&input,&maengde);
-   Ingredients = fopen("Ingredients.txt", "a");
-   fprintf(Ingredients, "%s, %.2lfg\n",input,maengde);
-   fclose(Ingredients);
+    Ingredients = fopen("Ingredients.txt", "a");
+    fprintf(Ingredients, "%s, %.2lfg\n",input,maengde);
+    fclose(Ingredients);
 
-   Ingredients = fopen("Ingredients.txt", "r");
-   while(fgets(ingredients, 1000, Ingredients)){
+    Ingredients = fopen("Ingredients.txt", "r");
+
+    while(fgets(ingredients, 1000, Ingredients)){
         printf("%s", ingredients);
-   }
-   fclose(Ingredients);
+    }
+    fclose(Ingredients);
     return 0;
 }
