fdef0944ccb5090010c09240d7fd6e8acca6054f
Oliver wahl <oliver@avfager.dk>
2025-10-30 14:00:59 +0100

    30/10/2025

--- Main.c
+++ Main.c
@@ -1,41 +1,72 @@
 #include <stdio.h>
 #include <string.h>
-
+void addingredient();
+int split_ingredients(FILE* ingredientsFile, char localNames [] [1000], double localAmount []);
 int main() {
     FILE *ingredientsFile;
-    char input[1000];
-    char ingredients[1000];
-    char Target [1000];
+
+    int count;
     char ingredientNames [1000] [1000];
     double ingredientAmounts [1000];
-    double maengde=0;
-    int count=0;
+    int inputChoice = 2; 
+    scanf("%d",&inputChoice);
+    switch (inputChoice)
+    {
+    case 1 :
+          addingredient();
+        break;
+    case 2 : 
+     count = split_ingredients(ingredientsFile, ingredientNames, ingredientAmounts);
 
-    /*
-    printf("Write an ingredient and an amount in grams: \n");
-    scanf("%s%lf",&input,&maengde);
-    ingredientsFile = fopen("ingredientsFile.txt", "a");
-    fprintf(ingredientsFile, "%s, %.2lfg\n",input,maengde);
-    fclose(ingredientsFile);
-    */
-
-    ingredientsFile = fopen("ingredientsFile.txt", "r");
+        printf("count %d", count);
+      for (int i = 0; i < count; i++)
+      {
+        printf("%s %.2lfg\n", ingredientNames [i], ingredientAmounts [i]);
+      }
+      break;
+    default:
+        break;
+    }
+   
+   
+    
+    return 0;
+}
+ int split_ingredients(FILE* ingredientsFile, char localNames [] [1000], double localAmount [] ){
+ 
+char ingredients[1000];
+ int count=0;
+ ingredientsFile = fopen("ingredients.txt", "r");
 
     while(fgets(ingredients, sizeof(ingredients), ingredientsFile) && count < 1000){
         char name [1000];
         double amount;
+
         if(sscanf(ingredients, "%[^,], %lf", name, &amount) == 2) {
-        strcpy(ingredientNames [count], name);
-        ingredientAmounts [count] = amount;
+        strcpy(localNames [count], name);    
+        localAmount [count] = amount;
         count++;
         }
     }
     fclose(ingredientsFile);
-    printf("count %d", count);
-    for (int i = 0; i < count; i++)
-    {
-        printf("%s %.2lfg\n", ingredientNames [i], ingredientAmounts [i]);
-    }
-    
-    return 0;
+
+
+
+
+
+
+ return count;
+}
+
+void addingredient() {
+
+    int maengde; 
+    char input [1000]; 
+    FILE* ingredientsFile;
+  printf("Write an ingredient and an amount in grams: \n");
+    scanf("%s%lf",&input,&maengde);
+    ingredientsFile = fopen("ingredients.txt", "a");
+    fprintf(ingredientsFile, "%s, %.2lfg\n",input,maengde);
+    fclose(ingredientsFile);
+
 }
